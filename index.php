fc3004787c944d8d4777985807171fc54e79ba28
Oli Passey <oli@hedgerows.net>
2023-04-02 23:27:41 +0100

    fliers

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php'; // Adjust the path to the autoload file if needed
+require_once 'vendor/autoload.php';
 $getID3 = new getID3;
 ?>
 
@@ -29,7 +29,7 @@ $getID3 = new getID3;
             padding: 1rem;
         }
         .recording {
-            background-color: darkred;
+            /* Remove background-color: darkred; */
             border: 1px solid #dee2e6;
             border-radius: 5px;
             width: 400px;
@@ -41,16 +41,38 @@ $getID3 = new getID3;
             padding: 1rem;
             box-sizing: border-box;
             color: white;
+            /* Add these lines */
+            background-size: cover;
+            background-position: center;
+            position: relative;
         }
+        .overlay {
+            background-color: rgba(0, 0, 0, 0.5);
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 1;
+            padding: 1rem;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+        }
+
         .filename, .metadata {
             text-align: center;
-            word-wrap: break-word;
+            inline-size: 375px;
+            /* overflow: hidden; */
+            overflow-wrap: break-word;
         }
         audio {
             width: 100%;
         }
         .download-btn {
-            background-color: #007bff;
+            background-color: #830d0d;
             border: none;
             border-radius: 5px;
             color: white;
@@ -62,43 +84,66 @@ $getID3 = new getID3;
             margin: 0 1rem;
             cursor: pointer;
         }
+        .flier {
+            margin-bottom: 1rem;
+        }
     </style>
 </head>
 <body>
-<h1>Therapy Sessions Archive</h1>
-        <div id="recording-container">
-            <?php
-            $audio_dir = "audio/";
+    <h1>Therapy Sessions Recordings</h1>
+    <div id="recording-container">
+        <?php
+        $audio_dir = "audio/";
+        $fliers_dir = "fliers/";
+        $flier_extensions = ['jpg', 'jpeg', 'png', 'webp'];
 
-            if (is_dir($audio_dir)) {
-                $files = glob($audio_dir . "*.mp3");
-                foreach ($files as $file) {
-                    $file_name = basename($file);
-                    $file_info = $getID3->analyze($file);
-
-                    $artist = isset($file_info['tags']['id3v2']['artist'][0]) ? $file_info['tags']['id3v2']['artist'][0] : (isset($file_info['tags']['id3v1']['artist'][0]) ? $file_info['tags']['id3v1']['artist'][0] : 'Unknown');
-                    $title = isset($file_info['tags']['id3v2']['title'][0]) ? $file_info['tags']['id3v2']['title'][0] : (isset($file_info['tags']['id3v1']['title'][0]) ? $file_info['tags']['id3v1']['title'][0] : 'Unknown');
-                    $album = isset($file_info['tags']['id3v2']['album'][0]) ? $file_info['tags']['id3v2']['album'][0] : (isset($file_info['tags']['id3v1']['album'][0]) ? $file_info['tags']['id3v1']['album'][0] : 'Unknown');
-                    $year = isset($file_info['tags']['id3v2']['year'][0]) ? $file_info['tags']['id3v2']['year'][0] : (isset($file_info['tags']['id3v1']['year'][0]) ? $file_info['tags']['id3v1']['year'][0] : 'Unknown');
-
-
-                    echo "<div class=\"recording\">
-                            <div class=\"filename\">$file_name</div>
-                            <div class=\"metadata\">
-                                <p>Artist: $artist</p>
-                                <p>Title: $title</p>
-                                <p>Album: $album</p>
-                                <p>Year: $year</p>
-                            </div>
-                            <audio controls src=\"$file\"></audio>
-                            <button class=\"download-btn\" onclick=\"location.href='$file'\">Download</button>
-                            <span>0 downloads</span>
-                          </div>";
+        function find_matching_flier($filename, $fliers_dir, $flier_extensions) {
+            foreach ($flier_extensions as $ext) {
+                $possible_flier = $fliers_dir . str_replace('.mp3', ".$ext", $filename);
+                if (file_exists($possible_flier)) {
+                    return $possible_flier;
                 }
-            } else {
-                echo "<p>Error: the 'audio' directory does not exist.</p>";
             }
-            ?>
+            return null;
+        }
+
+        if (is_dir($audio_dir)) {
+            $files = glob($audio_dir . "*.mp3");
+            foreach ($files as $file) {
+                $file_name = basename($file);
+                $flier_path = find_matching_flier($file_name, $fliers_dir, $flier_extensions);
+                $flier = $flier_path ? "style=\"background-image: url('$flier_path');\"" : '';
+                $file_info = $getID3->analyze($file);
+        
+                $artist = isset($file_info['tags']['id3v2']['artist'][0]) ? $file_info['tags']['id3v2']['artist'][0] : (isset($file_info['tags']['id3v1']['artist'][0]) ? $file_info['tags']['id3v1']['artist'][0] : 'Unknown');
+                $title = isset($file_info['tags']['id3v2']['title'][0]) ? $file_info['tags']['id3v2']['title'][0] : (isset($file_info['tags']['id3v1']['title'][0]) ? $file_info['tags']['id3v1']['title'][0] : 'Unknown');
+                $album = isset($file_info['tags']['id3v2']['album'][0]) ? $file_info['tags']['id3v2']['album'][0] : (isset($file_info['tags']['id3v1']['album'][0]) ? $file_info['tags']['id3v1']['album'][0] : 'Unknown');
+                $year = isset($file_info['tags']['id3v2']['year'][0]) ? $file_info['tags']['id3v2']['year'][0] : (isset($file_info['tags']['id3v1']['year'][0]) ? $file_info['tags']['id3v1']['year'][0] : 'Unknown');
+        
+                $content = <<<HTML
+        <div class="filename">$file_name</div>
+        <div class="metadata">
+            <p>Artist: $artist</p>
+            <p>Title: $title</p>
+            <p>Album: $album</p>
+            <p>Year: $year</p>
         </div>
-    </body>
-    </html>
+        <audio controls src="$file"></audio>
+        <button class="download-btn" onclick="location.href='$file'">Download</button>
+        <span>0 downloads</span>
+        HTML;
+        
+                echo "<div class=\"recording\" $flier>
+                        <div class=\"overlay\">
+                            $content
+                        </div>
+                      </div>";
+            }
+        } else {
+            echo "<p>Error: the 'audio' directory does not exist.</p>";
+        }
+        
+        ?>
+    </div>
+</body>
+</html>
